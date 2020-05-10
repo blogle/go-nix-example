a90f46a2a35b597a9b67332e109093361a565d90
Brandon Ogle <brandon@standardcognition.com>
2020-05-09 17:06:52 -0700

    Relocated go files under src

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ buildGoModule rec {
   pname = "go-nix-example";
   version = "0.0.1";
 
-  src = ./.;
+  src = ./src;
   modSha256 = "09p5c2wvjgwcvy40m7hgarwldgga0cf5ym39sshvvhfkjx2nvngd";
   subPackages = [ "." ];
 }
